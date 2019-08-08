4ace42994c0965809ded139f02325c1558f78816
Tomas Labella Menito <tomas.menito@hexagonagriculture.com>
2019-08-08 13:48:58 -0300

    Implementation of pagination and hyperlink relationship

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -5,18 +5,19 @@ from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 class SnippetSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.username")
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
 
     class Meta:
         model = Snippet
-        fields = ["id", "title", "code", "linenos", "language", "style", "owner"]
+        fields = ["url", "id","highlight", "title", "code", "linenos", "language", "style", "owner"]
 
 
 class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(
-        many=True, queryset=Snippet.objects.all()
+    snippets = serializers.HyperlinkedRelatedField(
+        many=True, view_name='snippet-detail', read_only=True
     )
 
     class Meta:
         model = User
-        fields = ["id", "username", "snippets"]
+        fields = ["url", "id", "username", "snippets"]
 
--- snippets/urls.py
+++ snippets/urls.py
@@ -5,10 +5,10 @@ from snippets import views
 urlpatterns = [
     path('', views.api_root),
     path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
-    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
-    path('snippets/<int:pk>/highlight', views.SnippetHightlight.as_view()),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
+    path('snippets/<int:pk>/highlight', views.SnippetHightlight.as_view(), name='snippet-highlight'),
     path('users/', views.UserList.as_view(), name='user-list'),
-    path('users/<int:pk>/', views.UserDetail.as_view()),
+    path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
 ]
 
 
